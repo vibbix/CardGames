26f7de5de9e6fd28f10d5bed2f1f34aef1f9ae4b
Mark Beznos <mark.vibbix@gmail.com>
2016-11-14 19:06:47 -0500

    Added Hand & HandTest
    -Started to implement Hand class
    -Started to implement tests for the getSoftHandValues()

---
+++ src/test/java/HandTest.java
@@ -0,0 +1,38 @@
+import edu.wit.comp2000.group25.lists.Card;
+import edu.wit.comp2000.group25.lists.Enums.CardSuit;
+import edu.wit.comp2000.group25.lists.Enums.CardValue;
+import edu.wit.comp2000.group25.lists.Hand;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by beznosm on 11/14/2016.
+ */
+public class HandTest {
+    @Test
+    public void getSoftHandValuesAllAces(){
+        Hand hand = new Hand();
+        hand.enqueueCard(new Card(CardSuit.Clubs, CardValue.Ace));
+        hand.enqueueCard(new Card(CardSuit.Clubs, CardValue.Ace));
+        Assert.assertArrayEquals(new int[]{2, 12, 22}, hand.getSoftHandValues());
+    }
+    @Test
+    public void getSoftHandValuesNoAces(){
+        Hand hand = new Hand();
+        hand.enqueueCard(new Card(CardSuit.Clubs, CardValue.Ten));
+        hand.enqueueCard(new Card(CardSuit.Clubs, CardValue.Ten));
+        Assert.assertArrayEquals(new int[]{20}, hand.getSoftHandValues());
+    }
+    @Test
+    public void getSoftHandWithOneAce(){
+        Hand hand = new Hand();
+        hand.enqueueCard(new Card(CardSuit.Clubs, CardValue.Ace));
+        hand.enqueueCard(new Card(CardSuit.Clubs, CardValue.Ten));
+        Assert.assertArrayEquals(new int[]{11, 21}, hand.getSoftHandValues());
+    }
+    @Test
+    public void getSoftHandWithEmptyHand(){
+        Hand hand = new Hand();
+        Assert.assertArrayEquals(new int[]{0}, hand.getSoftHandValues());
+    }
+}
